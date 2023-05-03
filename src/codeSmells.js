385b99dddf3abbb4bea81610e1dbde4942b50498
Juan Macías <info@juanmacias.es>
2023-05-03 13:16:47 +0200

    Add codeSmells.js

---
+++ src/codeSmells.js
@@ -0,0 +1,10 @@
+function func1() {
+    let arr = ["a", "b", "c"];
+
+    let expectedValue = "b";
+    if (expectedValue in arr) { // Noncompliant, will be always false
+        return expectedValue + " found in the array";
+    } else {
+        return expectedValue + " not found";
+    }
+}
