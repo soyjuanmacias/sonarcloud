f92ed6c4146c651970a9a57fbe88271a04c46220
Juan Macías <info@juanmacias.es>
2023-05-03 11:40:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sonar Cloud POC
+
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=soyjuanmacias_sonar-cloud&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=soyjuanmacias_sonar-cloud)
+
---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Código con comentarios
+ */
+
+const getPets = () => {
+    const isVet = true;
+
+    if(isVet) {
+        throw new Error("This error will never fire");
+    } else {
+        return {
+            dogs: ['Mia', 'Luna', 'Pedrito'],
+            cats: ['Bule', 'Michi', 'Miaurl'],
+        }
+    }
+};
+
+getPets();
+
+const getAge = (year) => 2023 - year;
+
+getAge(1992);
+getAge(1996);
+
+export default getAge;
+
+getAge(1966);
+
+const getRightWord = (word) => {
+    switch(word) {
+        case "LOLA": return "Lola Flores";
+        case "FARY": return "El Fary"
+    }
+}
+
+getRightWord("LOLA");
+getRightWord("FARY");
