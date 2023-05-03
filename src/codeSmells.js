0bbfeb4b26323d96030026d418f48f6c2c2c8b09
Juan Macías <info@juanmacias.es>
2023-05-04 00:08:44 +0200

    Add code

--- src/codeSmells.js
+++ src/codeSmells.js
@@ -1,10 +1,12 @@
 function func1() {
-    let arr = ["a", "b", "c"];
+let arr = ["a", "b", "c"];
 
-    let expectedValue = "b";
-    if (expectedValue in arr) { // Noncompliant, will be always false
-        return expectedValue + " found in the array";
-    } else {
-        return expectedValue + " not found";
-    }
+let expectedValue = "b";
+if (expectedValue in arr) { // Noncompliant, will be always false
+    return expectedValue + " found in the array";
+} else {
+    return expectedValue + " not found";
 }
+}
+
+func1();
--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@
  */
 
 const getPets = () => {
-    const isVet = true;
+	const isVet = true;
 
-    if(isVet == true) {
-        throw new Error("This error will never fire");
-    } else {
-        return {
-            dogs: ['Mia', 'Luna', 'Pedrito'],
-            cats: ['Bule', 'Michi', 'Miaurl'],
-        }
-    }
+	if (isVet == true) {
+		throw new Error('This error will never fire');
+	} else {
+		return {
+			dogs: ['Mia', 'Luna', 'Pedrito'],
+			cats: ['Bule', 'Michi', 'Miaurl'],
+		};
+	}
 };
 
 getPets();
 
-const getAge = (year) => 2023 - year;
+const getAge = year => 2023 - year;
 
 getAge(1992);
 getAge(1996);
@@ -26,13 +26,13 @@ export default getAge;
 
 getAge(1966);
 
-const getRightWord = (word) => {
-    switch(word) {
-        case "LOLA": return "Lola Flores";
-        case "FARY": return "El Fary"
-    }
-}
+const getRightWord = word => {
+	switch (word) {
+		case 'LOLA': return 'Lola Flores';
+		case 'FARY': return 'El Fary';
+	}
+};
 
-getRightWord("LOLA");
-getRightWord("FARY");
+getRightWord('LOLA');
+getRightWord('FARY');
 
